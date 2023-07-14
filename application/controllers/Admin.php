7eb24e05be74c41ed92cd7858bf8941a8d36e45d
mfikrid <111201911976@mhs.dinus.ac.id>
2023-07-14 17:53:11 +0700

    update

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,6 +6,7 @@ class Admin extends CI_Controller
     public function index()
     {
         $this->load->view('layout/header');
+        $this->load->view('layout/sidebar');
         $this->load->view('admin/dashboard');
         $this->load->view('layout/footer');
     }
