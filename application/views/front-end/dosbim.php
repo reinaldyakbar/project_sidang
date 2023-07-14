3e83375f7366ddfbb60eeb679448be04b3927b0d
mfikrid <111201911976@mhs.dinus.ac.id>
2023-07-14 23:14:34 +0700

    Update dosbim.php

--- application/views/front-end/dosbim.php
+++ application/views/front-end/dosbim.php
@@ -111,48 +111,6 @@
     </div>
 </section>
 
-
-<footer class="footer mt-5">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-5">
-                <img class="img-f" src="<?php echo base_url() ?>assets/front-end/assets/images/lgo.png" alt="...">
-            </div>
-            <div class="col-md-7 mt-sm-3">
-                <h3 class=" txt-footer">Kontak</h3>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="row">
-                            <div class="col-md-7 mt-sm-3">
-                                <ul class="list-unstyled txt-footer">
-                                    <li><a href="mailto:defri.kurniawan@dsn.ac.id"
-                                            class="text-white text-decoration-none"><i class="bi bi-envelope"></i>&nbsp
-                                            defri.kurniawan@dsn.ac.id</a>
-                                    </li>
-                                    <li><i class="bi bi-phone"></i> +6282225701985 </li>
-                                </ul>
-                            </div>
-                            <div class="col-md-5 col-foot">
-                                <ul class="list-unstyled txt-footer">
-                                    <li><a href="mailto:danang.wu@dsn.ac.id" class="text-white text-decoration-none"><i
-                                                class="bi bi-envelope"></i>&nbsp danang.wu@dsn.ac.id</li>
-                                    <a><i class="bi bi-phone"></i> +6285740955623</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-12">
-                        <ul class="list-unstyled txt-footer">
-                            <li><i class="bi bi-geo-alt" style="text-align: justify;"></i> Gedung H Lt. 2 Kampus
-                                Udinus <br>Jl. Imam Bonjol No.207 Pendrikan Kidul,<br> Kec. Semarang Tengah Kota
-                                Semarang,<br> Jawa Tengah 50131</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
 <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
 <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
