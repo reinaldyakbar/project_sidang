a2734d4fe63be32be1a39f4f847ab0657f09e4f2
wildanaldip <83572867+wildanaldip@users.noreply.github.com>
2023-07-20 13:27:00 +0700

    ppp

--- application/views/front-end/info_ta2.php
+++ application/views/front-end/info_ta2.php
@@ -9,70 +9,95 @@
             </div>
         </div>
     </div>
-    <div class="row d-flex justify-content-center">
-        <div class="col-lg-6 mx-4"
-            style="width: 500px; height: 350px; border-radius: 50px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
-            <div class="text-center mt-5 mb-5">
-                <h5 class="display-7 fw-bolder mb-3 "><span class=" d-inline ">Prosedur Tugas Akhir</span></h5>
-            </div>
-            <div class="row">
-                <div class="col-lg-12 px-5">
-                    <ol>
-                        <li>Pemilihan Dosbim</li>
-                        <li>Pemilihan topik penelitian</li>
-                        <li>Penyusunan proposal TA</li>
-                        <li>Penulisan laporan TA</li>
-                        <li>Sidang Akhir</li>
-                        <li>Yudisium</li>
-                    </ol>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-6 mx-4"
-            style="width: 500px; height: 350px; border-radius: 50px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
-            <div class="text-center mt-5 mb-5">
-                <h5 class="display-7 fw-bolder mb-3 "><span class=" d-inline ">Syarat Akademik</span></h5>
-            </div>
-            <div class="row">
-                <div class="col-lg-12 px-5">
-                    <ol>
-                        <li>Mahasiswa sudah menyelesaikan kuliah minimal 130 SKS
-                        </li>
-                        <li>Dalam daftar KHS tidak boleh ada nilai E</li>
-                        <li>IPK minimal 2.00</li>
-                        <li>Jumlah nilai D maksimal 10% dari jumlah mata kuliah yang telah diambil atau 4 mata
-                            kuliah</li>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="row d-flex justify-content-center">
-            <div class="col-lg-12 mx-4 mt-4"
-                style="width: 1050px; border-radius: 50px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
-                <div class="text-center mt-5 mb-5">
-                    <h5 class="display-7 fw-bolder mb-3 "><span class=" d-inline ">Syarat Sidang Akhir</span>
-                    </h5>
-                </div>
-                <div class="row">
-                    <div class="col-lg-12 px-5 mb-4">
+    <div class="d-flex justify-content-center mt-4">
+        <div class="accordion" id="accordionExample" style="width: 1050px;">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Pola dan Bentuk Tugas Akhir
+                    </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
                         <ol>
-                            <li>Menyerahkan dokumen laporan tugas akhir, sebanyak 4 eksemplar ke dosbing dan
-                                penguji setelah diplot</li>
-                            <li>Tidak boleh ada nilai E.</li>
+                            <li>Tugas Akhir Reguler
+                                <p>Dapat ditempuh dengan bermacam bentuk seperti:</p>
+                                <ul>
+                                    <li>Kajian Ilmiah</li>
+                                    <li>Kajian Prototipe</li>
+                                    <li>Perancangan Perangkat Informatika</li>
+                                    <li>Pengembangan Perangkat Informatika</li>
+                                </ul>
+                            </li>
+                            <li>Kuliah Kerja Industri (KKI)</li>
+                            <li>Publikasi Jurnal Internasional Bereputasi (Terindeks Scopus minim Q3) atau dengan Konferensi Internasional (terindex Scopus)</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                    <button class="accordion-button bg-white collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Syarat Administrasi Tugas Akhir 2
+                    </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <ol>
+                            <li>Mahasiswa tersebut terdaftar sebagai mahasiswa aktif</li>
+                            <li>Mahasiswa tersebut sudah menginput KRS Tugas Akhir 2</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingThree">
+                    <button class="accordion-button bg-white collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Syarat Akademik Tugas Akhir 2
+                    </button>
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <ol>
+                            <li>Mahasiswa sudah menyelesaikan kuliah minimal 130 SKS</li>
+                            <li>Dalam daftar KHS tidak boleh ada nilai E</li>
                             <li>IPK minimal 2.00</li>
-                            <li>Telah menyelesaikan kuliah minimal 140 SKS.</li>
-                            <li>Jumlah nilai D maksimal 5% dari jumlah mata kuliah yang telah diambil atau 2
-                                mata kuliah.</li>
+                            <li>Jumlah nilai D maksimal 10% dari jumlah mata kuliah yang telah diambil atau 4 mata kuliah</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingFour">
+                    <button class="accordion-button bg-white collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseThree">
+                        Syarat Sidang Tugas Akhir 2
+                    </button>
+                </h2>
+                <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <ol>
+                            <li>Mahasiswa harus lulus mata kuliah Tugas Akhir 1 minimal dengan nilai C</li>
+                            <li>Bimbingan minimal TA 2 8 kali</li>
+                            <li>Menyerahkan dokumen laporan tugas akhir, sebanyak 4 eksemplar</li>
+                            <li>Tidak boleh ada nilai E</li>
+                            <li>IPK minimal 2.00</li>
+                            <li>Telah menyelesaikan kuliah minimal 140 SKS</li>
+                            <li>Jumlah nilai D maksimal 5% dari jumlah mata kuliah yang telah diambil atau 2 mata kuliah</li>
                             <li>Skor TOEFL minimal 400</li>
-                            <li>Bebas beban administratif.</li>
-                            <li>Setifikat seminar dan atau workshop sebanyak minimal 2 buah</li>
+                            <li>Bebas beban administratif</li>
+                            <li>Sertifikat seminar nasional dan workshop masing-masing 1 buah</li>
+                            <li>Turnitin max 25%</li>
+                            <li>Pakaian yang digunakan saat Sidang TA 2, menggunakan baju putih berkerah,dasi,celana hitam, dan sepatu hitam serta jas almameter</li>
                         </ol>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+
 
 </section>
 <section class="bg-white py-5" style="margin-top: 8px; margin-bottom: 104px;">
@@ -84,3 +109,29 @@
         </div>
     </div>
 </section>
+<script>
+    // Dapatkan semua tombol accordion
+    const accordionButtons = document.querySelectorAll('[data-bs-toggle="collapse"]');
+
+    // Loop melalui setiap tombol accordion dan tambahkan event listener
+    accordionButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            // Dapatkan target collapse yang terkait dengan tombol ini
+            const targetId = this.getAttribute('data-bs-target');
+            const targetCollapse = document.querySelector(targetId);
+
+            // Dapatkan semua collapse yang ada di dalam parent accordion
+            const allCollapses = document.querySelectorAll('.accordion-collapse');
+
+            // Loop melalui setiap collapse dan sembunyikan jika bukan collapse yang sedang ditampilkan
+            allCollapses.forEach(collapse => {
+                if (collapse !== targetCollapse) {
+                    collapse.classList.remove('show');
+                }
+            });
+
+            // Toggle class "show" pada collapse untuk mengontrol tampilan
+            targetCollapse.classList.toggle('show');
+        });
+    });
+</script>
