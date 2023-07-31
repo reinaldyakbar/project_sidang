b55a61442770afcf14a2a4a56cb8c2bcd7f680d2
wildanaldip <83572867+wildanaldip@users.noreply.github.com>
2023-07-31 13:10:26 +0700

    pengumuman

--- application/views/front-end/pengumuman.php
+++ application/views/front-end/pengumuman.php
@@ -37,68 +37,76 @@
         /* Mengubah opasitas menjadi 1 saat dihover */
     }
 </style>
-<div class="container my-5">
-    <div class="card-info px-3 py-3 mb-3 " style="margin-top: 100px;">
-        <!-- Tautan ke halaman lain dengan menggunakan elemen <a> -->
-        <a href="<?php echo base_url('dashboard/isi_pengumuman') ?>" style="text-decoration: none; color: inherit;">
-            <div class="row d-flex justify-content-between">
-                <div class="col-lg-9">
-                    <h4>Info Pemilihan Dosen Pembimbing TA TI S1 Ganjil 2022/2023</h4>
-                    <p>Diberitahukan kepada mahasiswa program studi TI S-1 yang mengambil Tugas Akhir yang belum
-                        memiliki Dosen Pembimbing TA dapat mengajukan permohonan Dosen Pembimbing melalui Sistem
-                        Pengajuan Dosen Pembimbing (SPDP) TA di alamat https://tatis1.dinus.web.id/...
-                    </p>
-                    <hr>
-                    <p>By : Koordinator TA TI S-1</p>
-                </div>
-
-                <div class="col-lg-2">
-                    <p>12 September 2022</p>
-                </div>
+<section class="bg-white py-5" style="margin-top: 104px; margin-bottom: 8px;">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="text-center mb-5 mt-4">
+                <h2 class="display-7 fw-bolder mb-5 "><span class="text-gradient d-inline ">Pengumuman Tugas Akhir <br> Universitas Dian Nuswantoro</span></h2>
             </div>
+        </div>
     </div>
-</div>
-<div class="container my-5">
-    <div class="card-info px-3 py-3 mb-3 " style="margin-top: 100px;">
-        <!-- Tautan ke halaman lain dengan menggunakan elemen <a> -->
-        <a href="halaman_pertama.html" style="text-decoration: none; color: inherit;">
-            <div class="row d-flex justify-content-between">
-                <div class="col-lg-9">
-                    <h4>Info Pemilihan Dosen Pembimbing TA TI S1 Ganjil 2022/2023</h4>
-                    <p>Diberitahukan kepada mahasiswa program studi TI S-1 yang mengambil Tugas Akhir yang belum
-                        memiliki Dosen Pembimbing TA dapat mengajukan permohonan Dosen Pembimbing melalui Sistem
-                        Pengajuan Dosen Pembimbing (SPDP) TA di alamat https://tatis1.dinus.web.id/...
-                    </p>
-                    <hr>
-                    <p>By : Koordinator TA TI S-1</p>
-                </div>
+    <div class="container my-5">
+        <div class="card-info px-3 py-3 mb-3 " style="margin-top: 100px;">
+            <!-- Tautan ke halaman lain dengan menggunakan elemen <a> -->
+            <a href="<?php echo base_url('dashboard/isi_pengumuman') ?>" style="text-decoration: none; color: inherit;">
+                <div class="row d-flex justify-content-between">
+                    <div class="col-lg-9">
+                        <h4>Info Pemilihan Dosen Pembimbing TA TI S1 Ganjil 2022/2023</h4>
+                        <p>Diberitahukan kepada mahasiswa program studi TI S-1 yang mengambil Tugas Akhir yang belum
+                            memiliki Dosen Pembimbing TA dapat mengajukan permohonan Dosen Pembimbing melalui Sistem
+                            Pengajuan Dosen Pembimbing (SPDP) TA di alamat https://tatis1.dinus.web.id/...
+                        </p>
+                        <hr>
+                        <p>By : Koordinator TA TI S-1</p>
+                    </div>
 
-                <div class="col-lg-2">
-                    <p>12 September 2022</p>
+                    <div class="col-lg-2">
+                        <p>12 September 2022</p>
+                    </div>
                 </div>
-            </div>
-        </a>
+        </div>
     </div>
-    <div class="card-info px-3 py-3 mb-3">
-        <!-- Tautan ke halaman lain dengan menggunakan elemen <a> -->
-        <a href="halaman_pertama.html" style="text-decoration: none; color: inherit;">
-            <div class="row d-flex justify-content-between">
-                <div class="col-lg-9">
-                    <h4>Pengajuan Dosen Pembimbing TA/Skripsi TI S1 Semester Gasal 2021/2022</h4>
-                    <p>Diberitahukan kepada mahasiswa program studi Teknik Informatika S1 yang belum diplot/mendapatkan
-                        Dosen Pembimbing TA di siadin. Harap dapat mengajukan permohonannya melalui aplikasi web SPDP TA
-                        yang dapat...
-                    </p>
-                    <hr>
-                    <p>By : Koordinator TA TI S-1</p>
-                </div>
+    <div class="container my-5">
+        <div class="card-info px-3 py-3 mb-3 " style="margin-top: 100px;">
+            <!-- Tautan ke halaman lain dengan menggunakan elemen <a> -->
+            <a href="halaman_pertama.html" style="text-decoration: none; color: inherit;">
+                <div class="row d-flex justify-content-between">
+                    <div class="col-lg-9">
+                        <h4>Info Pemilihan Dosen Pembimbing TA TI S1 Ganjil 2022/2023</h4>
+                        <p>Diberitahukan kepada mahasiswa program studi TI S-1 yang mengambil Tugas Akhir yang belum
+                            memiliki Dosen Pembimbing TA dapat mengajukan permohonan Dosen Pembimbing melalui Sistem
+                            Pengajuan Dosen Pembimbing (SPDP) TA di alamat https://tatis1.dinus.web.id/...
+                        </p>
+                        <hr>
+                        <p>By : Koordinator TA TI S-1</p>
+                    </div>
 
-                <div class="col-lg-2">
-                    <p>07 September 2022</p>
+                    <div class="col-lg-2">
+                        <p>12 September 2022</p>
+                    </div>
                 </div>
-            </div>
-        </a>
+            </a>
+        </div>
+        <div class="card-info px-3 py-3 mb-3">
+            <!-- Tautan ke halaman lain dengan menggunakan elemen <a> -->
+            <a href="halaman_pertama.html" style="text-decoration: none; color: inherit;">
+                <div class="row d-flex justify-content-between">
+                    <div class="col-lg-9">
+                        <h4>Pengajuan Dosen Pembimbing TA/Skripsi TI S1 Semester Gasal 2021/2022</h4>
+                        <p>Diberitahukan kepada mahasiswa program studi Teknik Informatika S1 yang belum diplot/mendapatkan
+                            Dosen Pembimbing TA di siadin. Harap dapat mengajukan permohonannya melalui aplikasi web SPDP TA
+                            yang dapat...
+                        </p>
+                        <hr>
+                        <p>By : Koordinator TA TI S-1</p>
+                    </div>
+
+                    <div class="col-lg-2">
+                        <p>07 September 2022</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <!-- Tambahkan card-info lainnya dengan tautan ke halaman masing-masing -->
     </div>
-    <!-- Tambahkan card-info lainnya dengan tautan ke halaman masing-masing -->
-</div>
 </section>
