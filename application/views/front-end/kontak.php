d44dd8093a8ba414b0fb505be916a131954d6ec0
wildanaldip <83572867+wildanaldip@users.noreply.github.com>
2023-07-31 15:00:57 +0700

    kontak

--- application/views/front-end/kontak.php
+++ application/views/front-end/kontak.php
@@ -8,8 +8,7 @@
             </div>
         </div>
     </div>
-    <div class="container px-5 py-4 mb-2"
-        style="padding-top: 50px; padding-bottom: 50px; border-radius: 25px; height: auto; background: linear-gradient(to right, #0575e6, #021b79);">
+    <div class="container px-5 py-4 mb-2" style="padding-top: 50px; padding-bottom: 50px; border-radius: 25px; height: auto; background: linear-gradient(to right, #0575e6, #021b79);">
         <div class="row">
             <div class="col-lg-12 d-flex justify-content-center mb-4">
                 <h3 style="color: floralwhite;">Koordinator Tugas Akhir</h3>
@@ -18,10 +17,12 @@
                 <div class="row">
                     <div class="col-lg-6 d-flex align-items-center justify-content-center">
                         <ul class="list-unstyled txt-footer">
+                            <li>
+                                <h5>Defri Kurniawan M.Kom</h5>
+                            </li>
+                            <hr>
                             <li class="mb-2"><i class="bi bi-envelope"></i>
-                                <h5 class="d-inline" style="color: floralwhite;"><a
-                                        href="mailto:defri.kurniawan@dsn.ac.id"
-                                        class="text-white text-decoration-none text-gradient">defri.kurniawan@dsn.ac.id</a>
+                                <h5 class="d-inline" style="color: floralwhite;"><a href="mailto:defri.kurniawan@dsn.ac.id" class="text-white text-decoration-none text-gradient">defri.kurniawan@dsn.ac.id</a>
                                 </h5>
                             </li>
                             <li><i class="bi bi-phone"></i>
@@ -31,9 +32,12 @@
                     </div>
                     <div class="col-lg-6 d-flex align-items-center justify-content-center">
                         <ul class="list-unstyled txt-footer">
+                            <li>
+                                <h5> Danang Wahyu Utomo M.Kom</h5>
+                            </li>
+                            <hr>
                             <li class="mb-2"><i class="bi bi-envelope"></i>
-                                <h5 class="d-inline" style="color: floralwhite;"><a href="mailto:danang.wu@dsn.ac.id"
-                                        class="text-white text-decoration-none text-gradient">
+                                <h5 class="d-inline" style="color: floralwhite;"><a href="mailto:danang.wu@dsn.ac.id" class="text-white text-decoration-none text-gradient">
                                         danang.wu@dsn.ac.id</a></h5>
                             </li>
                             <li><i class="bi bi-phone"></i>
@@ -49,14 +53,12 @@
 
         </div>
     </div>
-    <div class="container px-5 py-5"
-        style="padding-top: 50px; padding-bottom: 50px; border-radius: 25px; height: auto; background: linear-gradient(to right, #0575e6, #021b79);">
+    <div class="container px-5 py-5" style="padding-top: 50px; padding-bottom: 50px; border-radius: 25px; height: auto; background: linear-gradient(to right, #0575e6, #021b79);">
         <div class="row">
             <div class="col-lg-12 d-flex align-items-center justify-content-between">
                 <ul class="list-unstyled txt-footer" style="margin-right: 100px; margin-left: 100px; margin-top: 17px;">
                     <li>
-                        <h4><a href="mailto:tatis1@dinus.web.id"
-                                class="text-white text-decoration-none text-gradient">Email : tatis1@dinus.web.id</a>
+                        <h4><a href="mailto:tatis1@dinus.web.id" class="text-white text-decoration-none text-gradient">Email : tatis1@dinus.web.id</a>
                         </h4>
                         <hr class="border-3">
                         <h4 style="color: floralwhite;">Gedung H Lt. 2 Kampus Udinus Jl. Imam Bonjol No.207
